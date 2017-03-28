f04a515060806bf74ba0f6d9f62a5dd12fd216df
Masa <masadordevic@gmail.com>
2017-03-28 20:04:16 +0200

    ycbcr (not working correctly)

---
+++ MMSlab/ColorModels.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMSlab
+{
+    public class RGB
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+
+        public RGB(byte r, byte g, byte b)
+        {
+            this.R = r;
+            this.B = b;
+            this.G = g;
+        }
+    }
+
+    public class YCbCr
+    {
+        public byte Y { get; set; }
+        public byte Cb { get; set; }
+        public byte Cr { get; set; }
+
+        public YCbCr(byte y, byte b, byte r)
+        {
+            this.Y = y;
+            this.Cb = b;
+            this.Cr = r;
+        }
+    }
+
+    public class ColorModels
+    {
+        public static RGB YCbCrToRGB(YCbCr ycbcr)
+        {
+            float r = Math.Max(0.0f, Math.Min(1.0f, (float)(ycbcr.Y + 1.4022 * (ycbcr.Cr-128))));
+            float g = Math.Max(0.0f, Math.Min(1.0f, (float)(ycbcr.Y - 0.3456 * (ycbcr.Cb-128) - 0.7145 * (ycbcr.Cr-128))));
+            float b = Math.Max(0.0f, Math.Min(1.0f, (float)(ycbcr.Y + 1.7710 * (ycbcr.Cb-128))));
+
+            return new RGB((byte)(r * 255), (byte)(g * 255), (byte)(b * 255));
+        }
+
+
+        public static YCbCr RGBtoYCbCr(RGB rgb)
+        {
+            byte resY = (byte)((0.299 * rgb.R) + (0.587 * rgb.G) + (0.114 * rgb.B));
+            byte resCb = (byte)(128 - (0.168736 * rgb.R) - (0.331264 * rgb.G) + (0.5 * rgb.B));
+            byte resCr = (byte)(128 + (0.5 * rgb.R) - (0.418688 * rgb.G) - (0.081312 * rgb.B));
+
+            return new YCbCr(resY, resCb, resCr);
+        }
+
+    }
+  
+
+}
--- MMSlab/Views/YcbCrView.cs
+++ MMSlab/Views/YcbCrView.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace MMSlab.Views
 {
@@ -14,6 +15,7 @@ namespace MMSlab.Views
     {
         #region Variables
         private Bitmap bitmap = null;
+        private Bitmap[] channels = new Bitmap[3];
         private double zoom = 0.2;
         #endregion
 
@@ -30,6 +32,11 @@ namespace MMSlab.Views
                 if (value == null) return;
                 this.AutoScrollMinSize = new Size((int)(value.Width * zoom), (int)(value.Height * zoom));
                 this.bitmap = value;
+                for (int i = 0; i < 3; i++)
+                {
+                    this.channels[i] = (Bitmap)value.Clone();
+                }
+
                 base.Invalidate();
             }
         }
@@ -48,15 +55,117 @@ namespace MMSlab.Views
             InitializeComponent();
         }
 
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            Invalidate();
+        }
+
         protected override void OnPaint(PaintEventArgs pevent)
         {
             var g = pevent.Graphics;
+
             if (this.Bitmap != null)
             {
-                g.DrawImage(this.Bitmap, new Rectangle(this.AutoScrollPosition.X, this.AutoScrollPosition.Y, (int)(this.Bitmap.Width * zoom), (int)(this.Bitmap.Height * zoom)));
+                int width = (this.ClientSize.Width - this.AutoScrollPosition.X) / 2;
+                int height = (this.ClientSize.Height - this.AutoScrollPosition.Y) / 2;
+
+                double scale = Math.Min((double)width / (double)this.Bitmap.Width, (double)height / (double)this.Bitmap.Height);
+
+                int imgWidth = (int)(this.Bitmap.Width * scale);
+                int imgHeight = (int)(this.Bitmap.Height * scale);
+                int wOffset = (int)((width - imgWidth) / 2.0);
+                int hOffset = (int)((height - imgHeight) / 2.0);
+                int wStart = this.AutoScrollPosition.X + wOffset;
+                int hStart = this.AutoScrollPosition.Y + hOffset;
+
+                this.SetChannels();
+
+                g.DrawImage(this.Bitmap, new Rectangle(wStart, hStart, imgWidth, imgHeight));
+                g.DrawImage(this.channels[0], new Rectangle(wStart + width, hStart, imgWidth, imgHeight));
+                g.DrawImage(this.channels[1], new Rectangle(wStart, hStart + height, imgWidth, imgHeight));
+                g.DrawImage(this.channels[2], new Rectangle(wStart + width, hStart + height, imgWidth, imgHeight));
+
             }
 
         }
-        
+
+        private void SetChannels()
+        {
+            Bitmap b = this.Bitmap;
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, b.PixelFormat);// PixelFormat.Format24bppRgb);
+
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+
+            BitmapData[] channelsBmData = new BitmapData[3];
+            System.IntPtr[] cScan0 = new IntPtr[3];
+            for (int i = 0; i < 3; i++)
+            {
+                channelsBmData[i] = this.channels[i].LockBits(new Rectangle(0, 0, this.channels[i].Width, this.channels[i].Height), ImageLockMode.ReadWrite, this.channels[i].PixelFormat);
+                cScan0[i] = channelsBmData[i].Scan0;
+            }
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                byte* Y = (byte*)(void*)cScan0[0];
+                byte* Cb = (byte*)(void*)cScan0[1];
+                byte* Cr = (byte*)(void*)cScan0[2];
+
+
+                int nOffset = stride - b.Width * 3;
+
+                byte red, green, blue;
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+                        blue = p[0];
+                        green = p[1];
+                        red = p[2];
+
+                        YCbCr ycbcr = ColorModels.RGBtoYCbCr(new RGB(red, green, blue));
+
+                        //Y
+                        RGB yRGB = new RGB(ycbcr.Y, ycbcr.Y, ycbcr.Y);
+                        Y[2] = yRGB.R;
+                        Y[1] = yRGB.G;
+                        Y[0] = yRGB.B;
+
+                        //Cb
+                        RGB cbRGB = ColorModels.YCbCrToRGB(new YCbCr(0, ycbcr.Cb, 0));
+                        Cb[2] = cbRGB.R;
+                        Cb[1] = cbRGB.G;
+                        Cb[0] = cbRGB.B;
+
+                        //Cr
+                        RGB crRGB = ColorModels.YCbCrToRGB(new YCbCr(0, 0, ycbcr.Cr));
+                        Cr[2] = crRGB.R;
+                        Cr[1] = crRGB.G;
+                        Cr[0] = crRGB.B;
+
+
+
+                        p += 3;
+                        Y += 3;
+                        Cb += 3;
+                        Cr += 3;
+                    }
+                    p += nOffset;
+                    Y += nOffset;
+                    Cb += nOffset;
+                    Cr += nOffset;
+                }
+            }
+
+            b.UnlockBits(bmData);
+            for (int i = 0; i < 3; i++)
+            {
+                this.channels[i].UnlockBits(channelsBmData[i]);
+            }
+        }
+
     }
 }
