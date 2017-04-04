39c12847c800456b9b351e35b01827e260b31ecc
Masa <masadordevic@gmail.com>
2017-04-04 16:16:13 +0200

    list extention

---
+++ MMSlab/Extentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMSlab
+{
+    public static class Extentions
+    {
+        public static void Push<T>(this List<T> list, T el)
+        {
+            if(list.Capacity == list.Count)
+            {
+                list.RemoveAt(list.Count - 1);
+            }
+
+            list.Insert(0, el);
+        }
+        
+    }
+}
